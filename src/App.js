79803daa25609836cb117649bf5d7c1fb82c11e2
김우빈 <kwb020312@naver.com>
2023-12-24 23:42:26 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+import * as tf from "@tensorflow/tfjs";
+import * as speech from "@tensorflow-models/speech-commands";
 
 function App() {
+  const [model, setModel] = useState(null);
+  const [action, setAction] = useState(null);
+  const [labels, setLabels] = useState(null);
+
+  const loadModel = async () => {
+    const recognizer = await speech.create("BROWSER_FFT");
+    console.log("Model Loaded");
+    await recognizer.ensureModelLoaded();
+    console.log(recognizer.wordLabels());
+  };
+
+  useEffect(() => {
+    loadModel();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
